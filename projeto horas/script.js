5bb2dea626f03cce3ed6fac6c405f335de087bce
eduardooliveira <eduardo.limaoliveira.50@gmail.com>
2024-02-29 10:44:44 -0400

    colocada a função para armazenar localmente no navegador as informações

--- projeto horas/script.js
+++ projeto horas/script.js
@@ -4,8 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const totalMissing = document.getElementById("total-missing");
     const totalBalance = document.getElementById("total-balance");
     const daySummaryList = document.getElementById("day-summary-list");
+    const resetButton = document.getElementById("reset-button");
 
-    let hoursByDay = {};
+    let hoursByDay = loadFromLocalStorage() || {};
 
     form.addEventListener("submit", function(event) {
         event.preventDefault();
@@ -28,6 +29,15 @@ document.addEventListener("DOMContentLoaded", function() {
         }
 
         updateBalance();
+        saveToLocalStorage();
+    });
+
+    resetButton.addEventListener("click", function() {
+        if (confirm("Tem certeza que deseja zerar as informações?")) {
+            hoursByDay = {};
+            updateBalance();
+            saveToLocalStorage();
+        }
     });
 
     function updateBalance() {
@@ -61,10 +71,18 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function formatMinutes(totalMinutes) {
-        const sign = totalMinutes < 0 ? '-' : ''; // Adiciona o sinal para minutos negativos
         const hours = Math.floor(Math.abs(totalMinutes) / 60);
         const remainingMinutes = Math.abs(totalMinutes) % 60;
+        const sign = totalMinutes < 0 ? '-' : ''; // Adiciona o sinal para minutos negativos
         return `${sign}${hours}h ${remainingMinutes < 10 ? '0' : ''}${remainingMinutes}min`;
     }
-    
+
+    function saveToLocalStorage() {
+        localStorage.setItem('hoursByDay', JSON.stringify(hoursByDay));
+    }
+
+    function loadFromLocalStorage() {
+        const data = localStorage.getItem('hoursByDay');
+        return data ? JSON.parse(data) : null;
+    }
 });
